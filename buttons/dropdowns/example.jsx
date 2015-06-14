59e410ae667a175ff62c1879b7e6762e2a2f5385
Daniel Worthington <danielworthington@gmail.com>
2015-06-14 00:07:27 -0700

    Added headers to menu examples

--- buttons/dropdowns/example.jsx
+++ buttons/dropdowns/example.jsx
@@ -5,6 +5,7 @@ const button = (
     <MenuItem eventKey='3' active={true}>Active Item</MenuItem>
     <MenuItem eventKey='4' disabled>Disabled Item</MenuItem>
     <MenuItem divider />
+    <MenuItem header>Header</MenuItem>
     <MenuItem eventKey='5'>Separated link</MenuItem>
   </DropdownButton>
 );
