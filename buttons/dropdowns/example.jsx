33f0ff4c61b59915f77ea0e12b301c0b1543b417
Daniel Worthington <danielworthington@gmail.com>
2015-06-14 00:05:55 -0700

    Added button dropdown and split button dropdown examples

---
+++ buttons/dropdowns/example.jsx
@@ -0,0 +1,12 @@
+const button = (
+  <DropdownButton bsStyle="{{style}}"{{openDirection}}{{alignment}}>
+    <MenuItem eventKey='1'>Action</MenuItem>
+    <MenuItem eventKey='2'>Another action</MenuItem>
+    <MenuItem eventKey='3' active={true}>Active Item</MenuItem>
+    <MenuItem eventKey='4' disabled>Disabled Item</MenuItem>
+    <MenuItem divider />
+    <MenuItem eventKey='5'>Separated link</MenuItem>
+  </DropdownButton>
+);
+
+React.render(button, document.body);
