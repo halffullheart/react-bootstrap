3a1e7bb95ba1ad72e25952a2f7bd59f35f9b46e4
Daniel Worthington <danielworthington@gmail.com>
2015-06-14 00:18:02 -0700

    Added button group example

---
+++ buttons/button-groups/example.jsx
@@ -0,0 +1,9 @@
+const buttonGroup = (
+  <ButtonGroup bsSize='{{size}}'>
+    <Button>Left</Button>
+    <Button>Middle</Button>
+    <Button>Right</Button>
+  </ButtonGroup>
+);
+
+React.render(buttonGroup, document.body);
