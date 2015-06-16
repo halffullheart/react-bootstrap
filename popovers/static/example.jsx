d54c456c4f9b4b103b4411a93ac73b5eb8239bce
Daniel Worthington <danielworthington@gmail.com>
2015-06-15 23:37:36 -0700

    Added static popover example

---
+++ popovers/static/example.jsx
@@ -0,0 +1,9 @@
+const popover = (
+  <div style={{ height: 120 }}>
+    <Popover placement='{{placement}}' positionLeft={200} positionTop={50} title='Popover {{placement}}'>
+      And here’s some <strong>amazing</strong> content. It’s very engaging. Right?
+    </Popover>
+  </div>
+);
+
+React.render(popover, document.body);
