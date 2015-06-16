325c77056e476527e104325bc18cbf7323bb3550
Daniel Worthington <danielworthington@gmail.com>
2015-06-15 19:59:18 -0700

    Added tooltip example

---
+++ tooltips/tooltips/example.jsx
@@ -0,0 +1,9 @@
+const tooltips = (
+  <ButtonToolbar>
+    <OverlayTrigger placement='{{placement}}' overlay={<Tooltip>Tooltip on the {{placement}}</Tooltip>}>
+      <Button>Tooltip on the {{placement}}</Button>
+    </OverlayTrigger>
+  </ButtonToolbar>
+);
+
+React.render(tooltips, document.body);
