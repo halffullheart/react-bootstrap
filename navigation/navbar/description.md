30f60351fbc1bab2d4f956fab4c29a14602d5e02
Daniel Worthington <danielworthington@gmail.com>
2015-06-17 23:26:03 -0700

    Added navbar example and description

---
+++ navigation/navbar/description.md
@@ -0,0 +1,11 @@
+### Navbar
+
+Navbars are responsive meta components that serve as navigation headers for your application or site. They begin collapsed (and are toggleable) in mobile views and become horizontal as the available viewport width increases.
+
+[Bootstrap documentation for navbar][bootstrap docs]
+
+[React Bootstrap documentation for navbar][react docs]
+
+
+[bootstrap docs]: http://getbootstrap.com/components/#navbar
+[react docs]: http://react-bootstrap.github.io/components.html#navbars
---
+++ navigation/navbar/example.jsx
@@ -0,0 +1,19 @@
+const navbar = (
+  <Navbar brand={{brand}}{{fixedTop}}{{fixedBottom}}{{staticTop}}{{fluid}}{{inverse}}>
+    <Nav{{rightLinks}}>
+      <NavItem eventKey={1} href='#'>Link</NavItem>
+      <NavItem eventKey={2} href='#'>Link</NavItem>
+    </Nav>
+    <Nav{{rightMenu}}>
+      <DropdownButton eventKey={3} title='Dropdown'>
+        <MenuItem eventKey='1'>Action</MenuItem>
+        <MenuItem eventKey='2'>Another action</MenuItem>
+        <MenuItem eventKey='3'>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey='4'>Separated link</MenuItem>
+      </DropdownButton>
+    </Nav>
+  </Navbar>
+);
+
+React.render(navbar, document.body);
