0acaec49f8e61966e43dd443d912f21c26d5a131
Daniel Worthington <danielworthington@gmail.com>
2015-06-17 21:17:53 -0700

    Added navigation nav
    nav = tabs/pills

---
+++ navigation/description.md
@@ -0,0 +1,11 @@
+## Navs
+
+Navs available in Bootstrap have shared markup, starting with the base .nav class, as well as shared states. Swap modifier classes to switch between each style.
+
+[Boostrap Documentation for navs][bootstrap docs]
+
+[React Boostrap Documentation for navs][react docs]
+
+
+[bootstrap docs]: http://getbootstrap.com/components/#nav
+[react docs]: http://react-bootstrap.github.io/components.html#navs
---
+++ navigation/tabs/example.jsx
@@ -0,0 +1,28 @@
+var ExampleNav = React.createClass({
+  getInitialState: function() {
+    return {
+      selectedItem: 1
+    };
+  },
+  handleSelect: function(key) {
+    this.setState({selectedItem: key});
+  },
+  render: function() {
+    return (
+      <Nav bsStyle='{{style}}'{{stacked}}{{justified}} activeKey={this.state.selectedItem} onSelect={this.handleSelect}>
+        <NavItem eventKey={1} href='/home'>NavItem 1 content</NavItem>
+        <NavItem eventKey={2} title='Item'>NavItem 2 content</NavItem>
+        <NavItem eventKey={3} disabled={true}>NavItem 3 content</NavItem>
+        <DropdownButton eventKey={4} title='Dropdown' navItem={true}>
+          <MenuItem eventKey='4.1'>Action</MenuItem>
+          <MenuItem eventKey='4.2'>Another action</MenuItem>
+          <MenuItem eventKey='4.3'>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey='4.4'>Separated link</MenuItem>
+        </DropdownButton>
+      </Nav>
+    );
+  }
+});
+
+React.render(<ExampleNav />, document.body);
