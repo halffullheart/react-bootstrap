6be04985ec81daaa5dd1a85d6831791f3864d215
Daniel Worthington <danielworthington@gmail.com>
2015-06-20 19:14:39 -0700

    Added pagination example and description

---
+++ pagination/pagination/description.md
@@ -0,0 +1,8 @@
+### Default Pagination
+
+Provide pagination links for your site or app with the multi-page pagination component. Set items to the number of pages. activePage prop dictates which page is active
+
+[React Bootstrap documentation for pagination][react docs]
+
+
+[react docs]: http://react-bootstrap.github.io/components.html#pagination
---
+++ pagination/pagination/example.jsx
@@ -0,0 +1,18 @@
+const PaginationExample = React.createClass({
+  getInitialState() {
+    return {
+      activePage: 1
+    };
+  },
+
+  handleSelect(event, selectedEvent){
+    this.setState({
+      activePage: selectedEvent.eventKey
+    });
+  },
+
+  render() {
+    return (
+      <Pagination{{previous}}{{next}}{{first}}{{last}}{{ellipsis}}{{totalItems}}{{maxButtons}} bsSize='{{size}}' items={10} activePage={this.state.activePage} onSelect={this.handleSelect} />
+
+React.render(<PaginationExample />, document.body);
