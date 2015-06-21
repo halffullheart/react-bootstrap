5d02f21968dfd389648deca807f5d8134f981e08
Daniel Worthington <danielworthington@gmail.com>
2015-06-20 18:33:44 -0700

    Added togglable tabs example and description

---
+++ navigation/togglable-tabs/description.md
@@ -0,0 +1,8 @@
+### Togglable Tabs
+
+Add quick, dynamic tab functionality to transition through panes of local content, even via dropdown menus.
+
+[React Bootstrap documentation for togglable tabs][react docs]
+
+
+[react docs]: http://react-bootstrap.github.io/components.html#tabs
---
+++ navigation/togglable-tabs/example.jsx
@@ -0,0 +1,9 @@
+const tabbedArea = (
+  <TabbedArea defaultActiveKey={2}{{animate}}>
+    <TabPane eventKey={1} tab='Tab 1'>TabPane 1 content</TabPane>
+    <TabPane eventKey={2} tab='Tab 2'>TabPane 2 content</TabPane>
+    <TabPane eventKey={3} tab='Tab 3' disabled={true}>TabPane 3 content</TabPane>
+  </TabbedArea>
+);
+
+React.render(tabbedArea, document.body);
