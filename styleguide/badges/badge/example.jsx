c10de34fdab69195d4173fb8d6888acc5497a608
Daniel Worthington <danielworthington@gmail.com>
2015-06-22 13:43:17 -0700

    Adde badge example and description

---
+++ styleguide/badges/badge/example.jsx
@@ -0,0 +1,21 @@
+const badges = (
+  <div>
+    <p>
+      <a href='#'>Messages <Badge>{{badgeContent}}</Badge></a>
+    </p>
+
+    <p>
+      <Button>Messages <Badge>{{badgeContent}}</Badge></Button>
+    </p>
+
+    <p>
+      <Nav bsStyle='tabs' activeKey={1}>
+        <NavItem eventKey={1}>Home</NavItem>
+        <NavItem eventKey={2}>Profile</NavItem>
+        <NavItem eventKey={3}>Messages <Badge>{{badgeContent}}</Badge></NavItem>
+      </Nav>
+    </p>
+  </div>
+);
+
+React.render(badges, document.body);
---
+++ styleguide/badges/description.md
@@ -0,0 +1,11 @@
+## Badges
+
+Easily highlight new or unread items by adding a badge to links, navs, and more.
+
+[Bootstrap documentation for badges][bootstrap docs]
+
+[React Bootstrap documentation for badges][react docs]
+
+
+[bootstrap docs]: http://getbootstrap.com/components/#badges
+[react docs]: http://react-bootstrap.github.io/components.html#badges
