fec85f840bb28405e5aaa2e5edef7be618e9d466
Daniel Worthington <danielworthington@gmail.com>
2015-06-24 18:42:45 -0700

    Added form example

---
+++ styleguide/forms/form/example.jsx
@@ -0,0 +1,9 @@
+const form = (
+  <form>
+    <Input id='exampleFormInputName' type='text' label='Name' placeholder='Jane Doe' />
+    <Input id='exampleFormInputEmail' type='email' label='Email' placeholder='jane.doe@example.com' />
+    <Button type='submit'>Send Invitation</Button>
+  </form>
+);
+
+React.render(form, document.body);
