83dfd6bcd9ed03bdc25a8e2bcad040ca9f683630
Daniel Worthington <danielworthington@gmail.com>
2015-07-03 16:39:58 -0700

    Schema update: list groups

--- styleguide/list-groups/list-group/example.jsx
+++ styleguide/list-groups/list-group/example.jsx
@@ -1,10 +1,10 @@
 const listgroup = (
   <ListGroup>
-    <ListGroupItem[[linked]][[heading]]>Cras justo odio[[badge]]</ListGroupItem>
-    <ListGroupItem[[linked]][[active]][[disabled]]>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem[[linked]][[heading]] bsStyle=[[style]]>Morbi leo risus</ListGroupItem>
-    <ListGroupItem[[linked]][[heading]]>Porta ac consectetur ac</ListGroupItem>
-    <ListGroupItem[[linked]][[heading]]>Vestibulum at eros</ListGroupItem>
+    <ListGroupItem [[itemAttrs]]>Cras justo odio [[badge]]</ListGroupItem>
+    <ListGroupItem [[itemAttrs]] [[states]]>Dapibus ac facilisis in</ListGroupItem>
+    <ListGroupItem [[itemAttrs]] [[style]]>Morbi leo risus</ListGroupItem>
+    <ListGroupItem [[itemAttrs]]>Porta ac consectetur ac</ListGroupItem>
+    <ListGroupItem [[itemAttrs]]>Vestibulum at eros</ListGroupItem>
   </ListGroup>
 );
 
