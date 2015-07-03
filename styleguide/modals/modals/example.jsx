bce625fd63280a2505e5af5356fa23a3b458037c
Daniel Worthington <danielworthington@gmail.com>
2015-07-03 00:42:11 -0700

    Schema update: modals

--- styleguide/modals/modals/example.jsx
+++ styleguide/modals/modals/example.jsx
@@ -1,7 +1,7 @@
 const MyModal = React.createClass({
   render() {
     return (
-      <Modal {...this.props} title='Modal heading' bsSize="[[size]]" animation=[[animate]]>
+      <Modal {...this.props} title='Modal heading' [[modalAttrs]]>
         <div className='modal-body'>
           <h4>Overflowing text to show scroll behavior</h4>
           <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
