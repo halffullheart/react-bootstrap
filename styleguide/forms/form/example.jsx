ad51e9dff53d97bb7aa2724e6197d485c4d0ff23
Daniel Worthington <danielworthington@gmail.com>
2015-07-03 16:02:00 -0700

    Schema update: forms

--- styleguide/forms/form/example.jsx
+++ styleguide/forms/form/example.jsx
@@ -1,5 +1,5 @@
 const form = (
-  <form>
+  <form [[formAttrs]]>
     <Input id='exampleFormInputName' type='text' label='Name' placeholder='Jane Doe' />
     <Input id='exampleFormInputEmail' type='email' label='Email' placeholder='jane.doe@example.com' />
     <Button type='submit'>Send Invitation</Button>
